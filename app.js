b5a072bb908a46b5c4950ea769372ea74507ced0
Roberto Vazquez <robertoadriandzulvazquez@gmail.com>
2025-03-18 16:04:07 -0500

    Nuevas Vistas

--- app.js
+++ app.js
@@ -16,6 +16,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.render('home'); 
 });
+
+app.get('/acercaNosotros', (req, res) => {
+    res.render('acercaNosotros'); 
+});
+
+app.get('/precios', (req, res) => {
+    res.render('precios'); 
+});
+
+app.get('/contactanos', (req, res) => {
+    res.render('contactanos'); 
+});
+
 app.get('/dashboard', (req, res) => {
     const content = req.query.content || 'main'; // Si no hay "content", carga "main.ejs"
     res.render('dashboard', { content });
